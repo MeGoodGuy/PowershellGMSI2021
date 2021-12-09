72186b770d4b79e9f9952e29f108f79c2ef48310
MeGoodGuy <edou.olivier@gmail.com>
2021-12-09 12:43:10 +0100

    Sarting - exo 3

--- exos.ps1
+++ exos.ps1
@@ -55,7 +55,7 @@ Get-ChildItem | Where-Object { $_.Length -gt 15 }
 
 
 <#  vous devez trouver tous les fichiers de plus de 300 Mb et les mettres dans un fichier .csv #> 
-Get-ChildItem | Where-Object { $_.Length -gt 15 } | ForEach-Object { $_.Name } | Out-File -FilePath "./output.csv"
+Get-ChildItem | Where-Object { $_.Length -gt 15 } | ForEach-Object { @{ taille=$_.Length; nom=$_.Name } } | Export-Csv -Delimiter ";" -NoTypeInformation -Path "./output2.csv"
 
 
 
@@ -66,16 +66,65 @@ Get-ChildItem | Where-Object { $_.Length -gt 1992 } | ForEach-Object { $_.Name }
 
 
 <#  Écrire un programme qui écrit 500 fois « Je dois faire des sauvegardes régulières de mes fichiers. » #> 
+1..50 | % { Write-Host "Je dois faire des backups de mes serveurs régulièrement, et les tester" }
+
 
 <#  Écrire un programme qui affiche tous les nombres impairs entre 0 et 15000, par ordre croissant : « 1 3 5 7 ... 14995 14997 14999 » #> 
+1..50 | ? { ($_ % 2) -ne 0}
 
-<#  Écrire un programme qui affiche la table de multiplication par 13 #> 
+
+
+
+
+
+Write-Host $True
+Write-Host $False
+Write-Host $Null
+
+$True -eq $False
+$True -ne $False
+
+$False -and $True # -> false   &&
+$True -and $True # -> true
+
+$False -or $True # -> true     ||
+$True -or $True # -> true
+
+15 -gt 10  # -> true
+15 -lt 10  # -> false
+
+10 -lt 10  # -> false
+10 -le 10  # -> true
+
+
+
+
+
+
+<#  Écrire un programme qui affiche la table de multiplication par 13 #>
+1..9 | % { 13 * $_ }
 
 <#  Ecrire un programme qui demande un mot à l’utilisateur et qui affiche à l’écran le nombre de lettres de ce mot. #> 
+(Read-Host -Prompt "Donnez un mot").Length
 
 <#  Ecrire un programme qui demande un nombre entier à l’utilisateur. L’ordinateur affiche ensuite le message "Ce nombre est pair" ou "Ce nombre est impair" selon le cas. #> 
+(((Read-Host -Prompt "Donnez un nombre entier")%2) -eq 0) ? {il est pair} : {il est impair}
 
 <#  Ecrire un programme qui demande un nombre compris entre 10 et 20, jusqu’à ce que la réponse convienne. En cas de réponse supérieure à 20, on fera apparaître un message : « Plus petit ! », et inversement, « Plus grand ! » si le nombre est inférieur à 10.  #> 
+
+$ValeurSaisie = Read-Host -Prompt "Donnez un nombre entier entre 10 et 20"
+[int]$MaValeurConvertie = 0;
+
+if ([int]::TryParse($ValeurSaisie, [ref]$MaValeurConvertie)) {
+    Write-Host "j'ai reussi a parser/caster/convertir le format/type de donnée"
+} else {
+    Write-Host "j'ai PAS reussi "
+}
+
+
+
+
+$ValeurSaisie -is [int]
 
 <#  Ecrire un programme qui demande un nombre de départ, et qui ensuite affiche les dix nombres suivants. Par exemple, si l'utilisateur entre le nombre 17, le programme affichera les nombres de 18 à 27.  #> 
 
@@ -83,19 +132,6 @@ Get-ChildItem | Where-Object { $_.Length -gt 1992 } | ForEach-Object { $_.Name }
 
 
 
-
-
-$UserList = @(
-    @{ ID=1; Name="AAA" },
-    @{ ID=2; Name="BBB" },
-    @{ ID=3; Name="CCC" },
-    @{ ID=4; Name="DDD" },
-    @{ ID=5; Name="EEE" },
-    @{ ID=6; Name="FFF" }
-)
-
-
-$UserList | ForEach-Object -Parallel
 
 
 
