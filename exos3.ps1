6dc6c91acff748d87c10e2d16d21e126f0639ba3
MeGoodGuy <edou.olivier@gmail.com>
2021-12-09 14:07:13 +0100

    Added expected result

--- exos3.ps1
+++ exos3.ps1
@@ -1,5 +1,7 @@
 
+#${😃} = "😃"
 
+#Write-Host ${😃}
 
 $UserList = @(
     @{ ID=1; Name="AAA"; Group="GMSI2020"; OrgUnit="fr.cesi.gmsi" },
@@ -19,4 +21,28 @@ $UserList | ForEach-Object {
 
 
 
-   
+
+   # $GroupHash
+   # $GroupMap            -> C'est pareil, les Map, les Hash et les Dict
+   # $GroupDictionnary    -> clefs / valeur
+
+
+$GroupHash = @{
+    GMSI2020= @(
+        @{ ID=1; Name="AAA"; Group="GMSI2020"; OrgUnit="fr.cesi.gmsi" },
+        @{ ID=2; Name="BBB"; Group="GMSI2020"; OrgUnit="fr.cesi.gmsi" },
+        @{ ID=3; Name="CCC"; Group="GMSI2020"; OrgUnit="fr.cesi.gmsi" }
+    ) ;
+    GMSI2021= @(
+        @{ ID=4; Name="DDD"; Group="GMSI2021"; OrgUnit="fr.cesi.gmsi" },
+        @{ ID=5; Name="EEE"; Group="GMSI2021"; OrgUnit="fr.cesi.gmsi" },
+        @{ ID=5; Name="FFF"; Group="GMSI2021"; OrgUnit="fr.cesi.gmsi" }
+    ) ;
+    GMSI2022= @(
+        @{ ID=5; Name="GGG"; Group="GMSI2022"; OrgUnit="fr.cesi.gmsi" },
+        @{ ID=6; Name="HHH"; Group="GMSI2022"; OrgUnit="fr.cesi.gmsi" }
+    ) ;
+}
+
+
+# https://docs.microsoft.com/fr-fr/dotnet/api/system.collections.hashtable.containskey?view=net-6.0
