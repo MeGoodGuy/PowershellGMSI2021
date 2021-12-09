bc253114fb912a7c1d70602ca7692bf1a46ccaee
MeGoodGuy <edou.olivier@gmail.com>
2021-12-09 15:06:24 +0100

    Added groupUserList init

---
+++ listGroupUnique.ps1
@@ -0,0 +1,26 @@
+
+
+
+$UserList = @(
+    @{ ID=1; Name="AAA"; Group="GMSI2020"; OrgUnit="fr.cesi.gmsi" },
+    @{ ID=2; Name="BBB"; Group="GMSI2020"; OrgUnit="fr.cesi.gmsi" },
+    @{ ID=3; Name="CCC"; Group="GMSI2020"; OrgUnit="fr.cesi.gmsi" },
+    @{ ID=4; Name="DDD"; Group="GMSI2021"; OrgUnit="fr.cesi.gmsi" },
+    @{ ID=5; Name="EEE"; Group="GMSI2021"; OrgUnit="fr.cesi.gmsi" },
+    @{ ID=5; Name="FFF"; Group="GMSI2021"; OrgUnit="fr.cesi.gmsi" },
+    @{ ID=5; Name="GGG"; Group="GMSI2022"; OrgUnit="fr.cesi.gmsi" },
+    @{ ID=6; Name="HHH"; Group="GMSI2022"; OrgUnit="fr.cesi.gmsi" }
+)
+
+
+
+$UserList | % { $_.Group } | Select-Object -Unique
+
+
+$GroupUserList = @{ }
+
+$UserList | % { $_.Group } | Select-Object -Unique | % { 
+    $GroupUserList[$_] = @( ) 
+}
+
+$GroupUserList | Out-String
