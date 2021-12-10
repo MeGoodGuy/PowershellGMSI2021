eb3e7599dcbec4c04c5a6ecf76754875569e0bfd
MeGoodGuy <edou.olivier@gmail.com>
2021-12-10 08:36:43 +0100

    Added functions

---
+++ Exercices/ExosFunctions/MyFirstFuntion.ps1
@@ -0,0 +1,52 @@
+
+
+
+
+
+# Appuyer sur Ctrl+Spacebar, taper "function inline"
+function FunctionName ($OptionalParameters) {
+    
+}
+
+
+
+
+
+
+# Appuyer sur Ctrl+Spacebar, taper "function"
+function FunctionName {
+    param (
+        $OptionalParameters = ""
+    )
+    
+}
+
+
+
+
+
+
+
+# Appuyer sur Ctrl+Spacebar, taper "function advanced"
+function Verb-Noun {
+    [CmdletBinding()]
+    param (
+        
+    )
+    
+    begin {
+        
+    }
+    
+    process {
+        
+    }
+    
+    end {
+        
+    }
+}
+
+
+
+
